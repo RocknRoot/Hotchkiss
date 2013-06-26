b2858ec7a393d062726fc08dc0d3e4a689210651
TibshoOT <tib@rocknroot.org>
2013-06-26 12:02:15 +0200

    Update migrate.rb code to catch exceptions

--- lib/hotchkiss/cli/migrate.rb
+++ lib/hotchkiss/cli/migrate.rb
@@ -5,31 +5,40 @@ module Cli
     desc "reset", "migration reset (erase and migration up)"
     def reset
       init
-      Sequel::Migrator.run(@db, MG_DIR, :target => 0)
-      Sequel::Migrator.run(@db, MG_DIR)
+      try_catch_migrate do
+        Sequel::Migrator.run(@db, MG_DIR, :target => 0)
+        Sequel::Migrator.run(@db, MG_DIR)
+      end
     end
 
     desc "to", "Update migrations (up or down) to VERSION."
     def to
       init
-      version = ENV['VERSION'].to_i
-      raise "No VERSION was provided" if version.nil?
-      Sequel::Migrator.run(@db, MG_DIR, :target => version)
+      try_catch_migrate do
+        raise "No VERSION was provided" unless  ENV['VERSION']
+        version = ENV['VERSION'].to_i
+        Sequel::Migrator.run(@db, MG_DIR, :target => version)
+      end
     end
 
     desc "up", "Update migrations to latest available."
     def up
       init
-      Sequel::Migrator.run(@db, MG_DIR)
+      try_catch_migrate do
+        Sequel::Migrator.run(@db, MG_DIR)
+      end
     end
 
     desc "down", "Full migration downgrade (erase all data)."
     def down
       init
-      Sequel::Migrator.run(@db, MG_DIR, :target => 0)
+      try_catch_migrate do
+        Sequel::Migrator.run(@db, MG_DIR, :target => 0)
+      end
     end
 
     no_tasks do
+
       def init
         Sequel.extension :migration
         begin
@@ -39,6 +48,20 @@ module Cli
           abort "Are you in your app directory ?"
         end
       end
+
+      def try_catch_migrate
+        begin
+          yield
+        rescue NoMethodError => e
+          puts "Migration error:"
+          puts "Do you have some migration defined ?"
+          abort "You can have error with the name of your migration (timestamp_name.rb) - Avoid '-' character."
+        rescue Sequel::Migrator::Error, Exception => e
+          puts "Migration error:"
+          abort e.message
+        end
+      end
+
     end
 
   end # Migrate
